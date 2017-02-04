71519697c58ccb76a07dc61c962cd792d430db37
Ishan <ikushum@gmail.com>
2017-02-04 13:34:28 +0545

    steps in prime refactored

--- Algorithms/20_steps_in_prime.rb
+++ Algorithms/20_steps_in_prime.rb
@@ -1,4 +1,3 @@
 def step(g, m, n)
-  (m..n).each{|num| return [num, num+g] if Prime.prime?(num) && Prime.prime?(num+g)}
-  return nil
+  return nil unless (m..n).each{|num| return [num, num+g] if Prime.prime?(num) && Prime.prime?(num+g)} 
 end
